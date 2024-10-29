d0161dc7e35ad260d48decf80c088f1eec96fdb8
jettywing <125429415+jettywing@users.noreply.github.com>
2024-10-29 03:25:39 -0400

    mpv call testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,102 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, process::Command};
+
+use crossterm::event::{Event, KeyEvent};
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::Line,
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    DefaultTerminal, Frame,
+};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Enter => self.play(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn play(&mut self) {
+        Command::new("mpv")
+            .arg("--audio-display=no")
+            .arg("atoms-for-peace-4trmx.mp3")
+            .arg("--input-ipc-server=/tmp/mpvsocket")
+            .output()
+            .expect("failed to execute program");
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Track ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " play ".into(),
+            "<Enter>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+
+        Paragraph::new("playing...")
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
